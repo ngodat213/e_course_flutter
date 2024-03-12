3aa6d1d2ba2f88ea9e5ddeaa2000f1c2a479210b
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2024-03-12 14:07:07 +0700

    init project

---
+++ lib/utils/localization_managet.dart
@@ -0,0 +1,19 @@
+import 'package:e_course_flutter/managers/manager_key_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalizationManager {
+  static final LocalizationManager _instance = LocalizationManager._internal();
+  factory LocalizationManager() => _instance;
+  LocalizationManager._internal();
+
+  Future<void> saveLocalization(int lang) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(ManagerKeyStorage.language, lang);
+  }
+
+  Future<int> getLocalization() async {
+    final prefs = await SharedPreferences.getInstance();
+    final result = prefs.getInt(ManagerKeyStorage.language);
+    return result ?? 0;
+  }
+}
